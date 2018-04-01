e5b5e40c430954702722c5d7f9ab61c83d57a9d8
her <lihe92@outlook.com>
2018-04-01 10:24:39 +0800

    蓝本编写完毕

---
+++ app/main/__init__.py
@@ -0,0 +1,12 @@
+# 蓝本
+# 在蓝本中定义的路由处于休眠状态，直到蓝本注册到程序上后，路由才真正成为程序
+# 的一部分。使用位于全局作用域中的蓝本时，定义路由的方法几乎和单脚本程序一样。
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+# 程序的路由保存在包里的app/main/views.py
+# 错误处理程序保存在app/main/errors.py
+# 把路由和错误处理程序与蓝本关联起来
+# "from ." 代表从当前包中导入
+from . import views, errors
