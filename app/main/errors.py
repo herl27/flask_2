3c19c49903534c95b7a8643973739cad602f5947
her <lihe92@outlook.com>
2018-04-01 11:42:28 +0800

    增加一处说明

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,6 +2,7 @@
 from flask import render_template
 from . import main
 
+# 蓝本中要想注册程序全局的错误处理程序，必须使用app_errorhandler
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
