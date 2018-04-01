10c8632ecd28f20d3c952a574bf50e7e3748c8bd
her <lihe92@outlook.com>
2018-04-01 09:47:28 +0800

    工厂函数编写完毕

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
+# 创建扩展实例
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
@@ -14,3 +15,13 @@ def create_app(config_name):
     app = Flask(__name__)
     # app的配置利用app.config对象提供的from_object()方法获取
     app.config.from_object(config[config_name])
+    # app初始化
+    config[config_name].init_app(app)
+
+    # 初始化扩展
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    return app
