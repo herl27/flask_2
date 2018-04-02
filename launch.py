2c355b111ac5e5fb71e0e851c4398b467b60c896
her <lihe92@outlook.com>
2018-04-02 16:58:53 +0800

    增加测试命令行

--- launch.py
+++ launch.py
@@ -9,5 +9,12 @@ migrete = Migrate(app, db)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+@app.cli.command()
+def test():
+    '''Run the unit tests.'''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
     app.run()
