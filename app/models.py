a861aa0335927bab3d04a8f8353b735f20047c77
her <lihe92@outlook.com>
2018-04-02 09:12:44 +0800

    修改Role名字

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class Roles(db.Model):
+class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
