2cc25769bb69fcabaac0b51026895bf6c8a861bf
HASEE WSL <lihe92@outlook.com>
2018-04-10 07:44:16 +0800

    增加登陆、注册页面，及相应表单类

---
+++ app/auth/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('电子邮件', validators=[DataRequired(), Length(1, 64),
+        Email('请输入有效的邮箱地址')])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('保存登录信息')
+    submit = SubmitField('提交')
+
+class RegistrationFrom(FlaskForm):
+    email = StringField('电子邮件', validators=[DataRequired(), Length(1, 64),
+        Email('请输入有效的邮箱地址')])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64),
+        Regexp("^[A-Za-z][A-Za-z0-9\_]*$", flags=0, message= "用户名只能以字母开头，且只能包含字母、数字和下划线")])
+    password = PasswordField("密码", validators=[DataRequired(), EqualTo('password2',
+        message="两次密码不一致")])
+    password2 = PasswordField("确认密码", validators=[DataRequired()])
+    submit = SubmitField("注册")
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经注册')
+            
