1f939499a2573ff12da030528034f215d2590026
HASEE WSL <lihe92@outlook.com>
2018-04-10 15:13:05 +0800

    增加确认账户及单元测试

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from app import db
+from . import db, login_manager
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -18,7 +19,12 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
 
+    def __repr__(self):
+        return '<Role %r>' % self.username
+
+    # 密码支持
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -30,8 +36,23 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return '<Role %r>' % self.username
+    # 用户认证
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config.get('SECRET_KEY'), expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config.get('SECRET_KEY'))
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        db.session.commit()
+        return True
 
 @login_manager.user_loader
 def load_user(user_id):
